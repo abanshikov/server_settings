b2aba532694f68df04327d920bef35f6fd2109b4
abanshikov <abanshikov@yandex.ru>
2022-01-22 17:36:14 +0700

    Edit readme.md and add commands.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# server_settings
+# Settings server Debian/Ubuntu
+
+1. Installing
+```bash
+$ sudo apt-get install wget
+$ sh -c "$(wget https://raw.githubusercontent.com/abanshikov/server_settings/main/install.sh -O -)"
+```
+
+2. Install pluggins for vim
+```bash
+$ vim ~/.vimrc
+
+:PlugInstall
+```
+
+3. Update settings zsh
+```bash
+$ source ~/.zshrc
+```
--- install.sh
+++ install.sh
@@ -25,5 +25,41 @@ ufw allow OpenSSH &&
 ufw allow 18181/tcp &&
 ufw enable &&
 
+# Vim settings
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+wget -O ~/.vimrc https://raw.githubusercontent.com/abanshikov/server_settings/main/vimrc
+
+# Zsh settings
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)" &&
+chsh -s (which zsh) &&
+echo "ZSH_THEME=\"agnoster\"" >> ~/.zshrc &&
+echo " " >> ~/.zshrc &&
+wget -O ~/.zshrcbak https://raw.githubusercontent.com/abanshikov/server_settings/main/zshrc &&
+cat ~/.zshrcbak >> ~/.zshrc &&
+rm ~/.zshrcbak &&
+
+# Python settings (install v. 3.9.7)
+apt-get install git &&
+curl https://pyenv.run | bash &&
+echo " " >> ~/.zshrc &&
+echo "# settings for penv" >> ~/.zshrc &&
+echo "export PATH=\"\$HOME/.pyenv/shims:\$PATH\"" >> ~/.zshrc &&
+echo "export PATH=\"\$HOME/.pyenv/bin:\$PATH\"" >> ~/.zshrc &&
+echo "eval \"\$(pyenv init -)\"" >> ~/.zshrc &&
+echo "eval \"\$(pyenv virtualenv-init -)\"" >> ~/.zshrc &&
+echo " " >> ~/.zshrc &&
+echo "# settings for virtualenv" >> ~/.zshrc &&
+echo "alias venv=\"virtualenv venv && source venv/bin/activate\"" >> ~/.zshrc &&
+echo "alias sv=\"source venv/bin/activate\"" >> ~/.zshrc &&
+echo "alias dv=\"deactivate\"" >> ~/.zshrc &&
+source ~/.zshrc &&
+pyenv install 3.9.7 &&
+pyenv global 3.9.7 &&
+pip install -U pip &&
+pip install virtualenv &&
+echo "**********************************************************************" &&
+pyenv versions &&
+python -V &&
+pip -V
 
 
